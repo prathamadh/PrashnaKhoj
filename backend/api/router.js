060caf49b52bf724f039c5fd43c5f5083cb12f71
Ashim-Stha <ashimshrestha2384@gmail.com>
2023-10-04 13:27:45 +0545

    receivedLocAndCopied

--- backend/api/router.js
+++ backend/api/router.js
@@ -3,14 +3,13 @@ const sqlite3 = require("sqlite3").verbose();
 const fs = require("fs");
 const path = require("path");
 
-const db = new sqlite3.Database("final.db");
+const db = new sqlite3.Database("questionpaper20.db");
 
 const router = require("express").Router();
 // const db = require("../config/connectdb");
 
 router.post("/", (req, res) => {
   const { search } = req.body;
-  console.log(search);
   // function insertImage() {
   //   const name = "photp.jpg";
   //   const imagePath = path.join(__dirname, "photp.jpg");
@@ -33,30 +32,41 @@ router.post("/", (req, res) => {
 
   // const name = "photp.jpg";
 
-  db.get("SELECT data FROM images WHERE name = ?", [search], (err, row) => {
-    if (err) {
-      console.error(err.message);
-      return res.status(500).json({
-        success: false,
-        message: "Database connection error",
-      });
-    }
+  db.get(
+    "select imagecropped from ioecropedquestionpaper where imagecropped = ?",
+    [search],
 
-    if (row) {
-      const imagePath = path.join(__dirname, "Retrieved_image.jpg");
-      fs.writeFileSync(imagePath, row.data);
-      console.log(`Retrieved ${search} from the database.`);
-      return res.status(200).json({
-        success: true,
-        data: row.data,
-      });
-    } else {
-      console.log(`${search} not found in the database.`);
-      return res.status(200).json({
-        success: false,
-        message: "record not found",
-      });
+    (err, row) => {
+      if (err) {
+        console.error(err.message);
+        return res.status(500).json({
+          success: false,
+          message: "Database connection error",
+        });
+      }
+
+      if (row) {
+        // const imagePath = path.join(__dirname, "Retrieved_image.jpg");
+        // fs.writeFileSync(imagePath, row.data);
+        const imgLoc = row.imagecropped;
+
+        const imagePath = path.join(__dirname, imgLoc);
+        const imgRec = fs.readFileSync(imagePath);
+        fs.writeFileSync("rec.png", imgRec);
+
+        console.log(`Retrieved ${imgLoc} from the database.`);
+        return res.status(200).json({
+          success: true,
+          data: imgLoc,
+        });
+      } else {
+        console.log(`${search} not found in the database.`);
+        return res.status(200).json({
+          success: false,
+          message: "record not found",
+        });
+      }
     }
-  });
+  );
 });
 module.exports = router;
